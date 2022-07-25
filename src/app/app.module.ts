e5522afd5c9af51256eed976e8d6301d5aa81319
ArmandoDiazR <94326192+ArmandoDiazR@users.noreply.github.com>
2022-07-25 14:15:27 -0500

    firstCommit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { ProductoComponent } from './producto/producto.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    ProductoComponent
   ],
   imports: [
     BrowserModule
