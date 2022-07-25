855f7847c4bbc4e5f89e724d84f4d85031dc451a
ArmandoDiazR <94326192+ArmandoDiazR@users.noreply.github.com>
2022-07-25 18:22:07 -0500

    todayIsMonday

--- src/app/producto/producto.component.ts
+++ src/app/producto/producto.component.ts
@@ -7,6 +7,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductoComponent implements OnInit {
 
+  imageSrc = 'assets/images/anya.jpg';
+  imageAlt = 'Anya Kwayyy';
+
+
   constructor() { }
 
   ngOnInit(): void {
